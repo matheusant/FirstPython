2632a1c10c62dbc481ad5603fde0f9a6b32bc61e
Matheus Santos <matheusantos99@gmail.com>
2023-05-04 11:06:36 -0300

    utilizando list comprehension

--- forca.py
+++ forca.py
@@ -3,26 +3,35 @@ def play():
     print("Jogo de Forca")
     print("*******************")
 
-    secret_word = "banana"
+    secret_word = "banana".upper()
+    errors = 0
     hit = False
     hanged = False
 
     print(f"A palavra tem {len(secret_word)} letras\n")
-    hint = ""
+    hint = ["_" for letter in secret_word]
 
-    for i in range(len(secret_word)):
-        hint += "_"
-
-    print(hint)
     while not hit and not hanged:
+        print(hint, '\n')
         attempt = input("Digite uma letra ")
-        attempt = attempt.strip()
+        attempt = attempt.strip().upper()
 
-        index = 1
-        for letter in secret_word:
-            if attempt.lower() == letter.lower():
-                print(f'A letra chutada foi {attempt.upper()} e ela está na posição {index}')
-            index += 1
+        if attempt in secret_word:
+            index = 0
+            for letter in secret_word:
+                if attempt == letter:
+                    hint[index] = letter
+                index += 1
+        else:
+            errors += 1
+
+        hit = "_" not in hint
+        hanged = errors == 6
+
+    if hit:
+        print("Você acertou a palavra {}".format(secret_word))
+    else:
+        print("Você perdeu a palavra era {}".format(secret_word))
     print("Fim de jogo")
 
 
